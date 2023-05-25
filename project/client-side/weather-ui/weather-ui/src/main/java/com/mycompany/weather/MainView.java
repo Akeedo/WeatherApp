c5912a599ca8b9f50e5d7e3cc2734887e972d385
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-05-25 10:59:43 +0600

    Changed button function to fetch weather data

--- project/client-side/weather-ui/weather-ui/src/main/java/com/mycompany/weather/MainView.java
+++ project/client-side/weather-ui/weather-ui/src/main/java/com/mycompany/weather/MainView.java
@@ -5,6 +5,10 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.router.Route;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
 
 /**
  * The main view contains a button and a click listener.
@@ -14,8 +18,30 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+        Button button = new Button("Get Weather",
+            click -> {
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                        .url("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
+                        .build();
+                    Response response = client.newCall(request).execute();
+
+                    if(response.isSuccessful() && response.body() != null){
+                        String responseBody = response.body().string();
+                        JSONObject json = new JSONObject(responseBody);
+                        String forecast = json.getJSONObject("current_weather").get("temperature").toString();
+                        Notification.show("The current temperature in London is: " + forecast +" *C");
+                    } else{
+                        String errorMessage = "Failed to fetch weather data. HTTP Status Code: " + response.code();
+                        Notification.show(errorMessage);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Notification.show("An error occurred while fetching weather data. Please try again.");
+                }
+            });
         add(button);
     }
 }
