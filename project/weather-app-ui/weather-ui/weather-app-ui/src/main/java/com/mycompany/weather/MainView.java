a8c293889f292b23fdec9ed05f0520a0f91df4a0
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-05-26 13:24:05 +0600

    Add fetchLocations in MainView class

--- project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/MainView.java
+++ project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/MainView.java
@@ -13,9 +13,21 @@ import com.vaadin.flow.router.Route;
 @PWA(name = "My Application", shortName = "My Application")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+    private void fetchLocations(String cityName) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid=e6e1309d61b8430fe739ae6b30ff3dd2"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            String responseBody = response.body();
+            JSONArray json = new JSONArray(responseBody);
+            grid.setItems(json.toList().stream().map(o -> new WeatherData(new JSONObject((Map) o))));
+        } else {
+            String errorMessage = "Failed to fetch location data. HTTP Status Code: " + response.statusCode();
+            Notification.show(errorMessage);
+        }
     }
 }
