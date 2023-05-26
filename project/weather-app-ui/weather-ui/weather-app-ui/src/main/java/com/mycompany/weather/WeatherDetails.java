7ded0681ddf5fe2c63cfaacc76efe23f3e4c0083
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-05-27 05:24:52 +0600

    Add WeatherDetails page

---
+++ project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/WeatherDetails.java
@@ -0,0 +1,19 @@
+package com.mycompany.weather;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("WeatherDetails")
+public class WeatherDetails extends VerticalLayout {
+    public WeatherDetails(){
+        H1 headerText = new H1("Weather Details");
+        HorizontalLayout header = new HorizontalLayout(headerText);
+        header.setWidthFull();
+        header.setJustifyContentMode(JustifyContentMode.CENTER);
+        header.setDefaultVerticalComponentAlignment(Alignment.AUTO);
+        add(header);
+    }
+
+}
