9012d4437e7fd646ee1e9672f88ffef008253e08
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-05-27 09:33:40 +0600

    Add longitude and latitude variables

--- project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/WeatherDetails.java
+++ project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/WeatherDetails.java
@@ -5,8 +5,8 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("WeatherDetails")
-public class WeatherDetails extends VerticalLayout {
+    private String longitude;
+    private String latitude;
     public WeatherDetails(){
         H1 headerText = new H1("Weather Details");
         HorizontalLayout header = new HorizontalLayout(headerText);
