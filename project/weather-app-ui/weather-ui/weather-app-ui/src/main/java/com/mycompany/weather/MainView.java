1d14448486cd38b7fbd4365f9facc9e17322b21f
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-05-29 09:09:29 +0600

    refactor(MainView): remove unused import java.util.Map

--- project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/MainView.java
+++ project/weather-app-ui/weather-ui/weather-app-ui/src/main/java/com/mycompany/weather/MainView.java
@@ -18,7 +18,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.Map;
+
 
 /**
  * The main view contains a button and a click listener.
