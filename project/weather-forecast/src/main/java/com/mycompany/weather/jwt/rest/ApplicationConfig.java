19944d6dbeffd4d8ed667480722cbe8ea1e42871
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-09 18:56:09 +0600

    feature(ApplicationConfig): add static imports for ADMIN and USER from Constants

--- project/weather-forecast/src/main/java/com/mycompany/weather/jwt/rest/ApplicationConfig.java
+++ project/weather-forecast/src/main/java/com/mycompany/weather/jwt/rest/ApplicationConfig.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import static com.mycompany.weather.jwt.Constants.ADMIN;
+import static com.mycompany.weather.jwt.Constants.USER;
+
 @DeclareRoles({ADMIN,USER})
 @ApplicationPath(value="api")
 public class ApplicationConfig extends Application {
