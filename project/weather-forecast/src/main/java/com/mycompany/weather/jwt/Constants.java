9066270567e18caea0a8789ad02887c9b13a1df3
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-09 17:03:44 +0600

    feature(Constants): add Constants.java file to the project

--- project/weather-forecast/src/main/java/com/mycompany/weather/jwt/Constants.java
+++ project/weather-forecast/src/main/java/com/mycompany/weather/jwt/Constants.java
@@ -1,2 +1,14 @@
-package com.mycompany.weather.jwt;public class Constants {
+package com.mycompany.weather.jwt;
+
+public class Constants {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String BEARER = "Bearer ";
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+
+    public static final String USER = "ROLE_USER";
+
+    public static final int REMEMBERME_VALIDITY_SECONDS = 24 * 60 * 60; //24 hours
 }
