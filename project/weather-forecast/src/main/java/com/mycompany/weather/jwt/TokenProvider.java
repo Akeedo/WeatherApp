41fbcf8ac0318aef870775c6c283b77cfaf3ed08
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-09 18:42:24 +0600

    feature(TokenProvider): add import for PostConstruct annotation

--- project/weather-forecast/src/main/java/com/mycompany/weather/jwt/TokenProvider.java
+++ project/weather-forecast/src/main/java/com/mycompany/weather/jwt/TokenProvider.java
@@ -13,6 +13,7 @@ import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
 
 public class TokenProvider {
     static final String AUTHORITIES_KEY ="auth";
