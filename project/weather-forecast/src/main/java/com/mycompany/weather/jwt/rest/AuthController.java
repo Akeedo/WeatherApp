893540f0d6e54947f382034a1dbfeb88c0550c44
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-10 06:38:22 +0600

    feature(auth-controller): inject SecurityContext into AuthController

--- project/weather-forecast/src/main/java/com/mycompany/weather/jwt/rest/AuthController.java
+++ project/weather-forecast/src/main/java/com/mycompany/weather/jwt/rest/AuthController.java
@@ -2,4 +2,6 @@ package com.mycompany.weather.jwt.rest;
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
+    @Inject
+    private SecurityContext securityContext;
 }
