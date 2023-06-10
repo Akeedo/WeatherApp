8f4fa49f9dee61a8cd388929950e754c2aec2841
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-10 06:56:08 +0600

    feature(sample-controller): import Logger and LoggerFactory into SampleController

--- project/weather-forecast/src/main/java/com/mycompany/weather/jwt/rest/SampleController.java
+++ project/weather-forecast/src/main/java/com/mycompany/weather/jwt/rest/SampleController.java
@@ -1,4 +1,8 @@
 package com.mycompany.weather.jwt.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.json.Json;
