2a7f08ebd965cb724bc5f241ae1ce6114876844c
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-13 05:54:04 +0600

    feature(city-resource): add getCity endpoint to CityResource class

--- project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/CityResource.java
+++ project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/CityResource.java
@@ -1,3 +1,12 @@
 package com.mycompany.rest;
 public class CityResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public City getCity(){
+        City city = new City();
+        city.setName("New York");
+        city.setCountry("USA");
+        city.setPopulation(860000);
+        return city;
+    }
 }
