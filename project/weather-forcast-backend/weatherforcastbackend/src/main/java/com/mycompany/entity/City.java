951f5bd3dabc6dfd02eee42fc49575f24b3b4a58
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-13 04:59:27 +0600

    feature(city-entity): import AllArgsConstructor, Getter, NoArgsConstructor, and Setter into City class

--- project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/entity/City.java
+++ project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/entity/City.java
@@ -1,4 +1,10 @@
 package com.mycompany.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
 @NoArgsConstructor
