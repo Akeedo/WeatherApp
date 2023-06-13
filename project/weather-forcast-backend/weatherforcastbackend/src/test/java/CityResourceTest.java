49c278671abb04af137b08781614a567eb417665
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-13 08:49:49 +0600

    feature(city-resource-test): add cityResource instance to CityResourceTest class

--- project/weather-forcast-backend/weatherforcastbackend/src/test/java/CityResourceTest.java
+++ project/weather-forcast-backend/weatherforcastbackend/src/test/java/CityResourceTest.java
@@ -1,4 +1,11 @@
 import com.mycompany.rest.CityResource;
 public class CityResourceTest {
     private CityResource cityResource;
+
+    @Before
+    public void setup(){
+        cityResource = new CityResource();
+    }
+
+    @Test
 }
