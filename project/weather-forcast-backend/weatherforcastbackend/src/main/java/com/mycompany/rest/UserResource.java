c3f4a3b7e86a8557ad5da8c17abe3a4856d65225
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-13 06:09:06 +0600

    feature(user-resource): add getUser endpoint to UserResource class

--- project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/UserResource.java
+++ project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/UserResource.java
@@ -1,3 +1,11 @@
 package com.mycompany.rest;
 public class UserResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(){
+        User user = new User();
+        user.setUsername("Akeed Anjum");
+        user.setPassword("Akeedo55 Eee");
+        return user;
+    }
 }
