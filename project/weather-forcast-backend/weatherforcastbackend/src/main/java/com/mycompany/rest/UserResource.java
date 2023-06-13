a20c38150ecc4fcc4525689dfad619e5db3d5ee9
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-13 06:12:29 +0600

    feature(user-resource): import GET, Path, Produces, and MediaType into UserResource class

--- project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/UserResource.java
+++ project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/UserResource.java
@@ -1,4 +1,10 @@
 package com.mycompany.rest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
 @Path("/user")
 public class UserResource {
     @GET
