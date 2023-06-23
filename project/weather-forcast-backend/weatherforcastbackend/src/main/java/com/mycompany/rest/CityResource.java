90103099380dd1504ff9d22a7361e19e7f487b66
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-06-24 00:58:48 +0600

    feature(CityResource): add getCities method with query parameters and exception handling

--- project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/CityResource.java
+++ project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/rest/CityResource.java
@@ -1,24 +1,40 @@
 package com.mycompany.rest;
 
-import com.mycompany.entity.City;
+
+import com.mycompany.service.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 
 @Path("/city")
 public class CityResource {
+
     private final Logger log = LoggerFactory.getLogger(CityResource.class);
+
     @Autowired
     private CityService cityService;
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public City getCity(){
-        City city = new City();
-        city.setName("New York");
-        city.setCountry("USA");
-        city.setPopulation(860000);
-        return city;
+    public Response getCities(@QueryParam("size") int size,
+                              @QueryParam("offset") int offset,
+                              @QueryParam("searchText") String searchText
+    ){
+        try{
+            log.info("Request received for application list by: {}", CityResource.class);
+            Response response =  cityService.getCities();
+            log.info("Response sent for City list : {}", response);
+            return response;
+        }
+        catch (WebApplicationException ex){
+            throw new WebApplicationException(ex.getMessage(), ex.getResponse().getStatus());
+        }
+
     }
 }
