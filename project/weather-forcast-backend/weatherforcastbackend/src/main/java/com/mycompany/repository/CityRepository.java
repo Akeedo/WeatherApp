6faeb10f0ce4b9aff7fdb39eea80d3ac39108c46
Akeed_Anjum <akeedanjumoffice@gmail.com>
2023-07-04 01:32:34 +0600

    feature(server-backend): remove Jersey, transition to Jetty server backend

--- project/weather-forcast-backend/weatherforcastbackend/src/main/java/com/mycompany/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.repository;
-
-import com.mycompany.entity.City;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends CrudRepository<City, String> {
-}
